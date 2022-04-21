a2dcd31d544cba9463b1eddb7981024986444821
jawad <jawadusaid11@gmail.com>
2022-04-21 14:47:03 +0530

    Spring Product

---
+++ ProductModule/src/main/java/com/example/product/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.product;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ProductRepository extends JpaRepository<Product, Integer>
+{
+}
